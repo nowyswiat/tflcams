e9f848d7502fbf9d3b57bd2ae566d347718f0939
nowyswiat <nowyswiat@users.noreply.github.com>
2017-06-30 19:24:09 +0100

    Latest

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-tflcams - view traffic cameras around London
-work in progress
+# tflcams
+
+An HTML5 application to view Transport for London (TfL) traffic cameras around London.
+
+[tflcams.ldsys.co.uk](http://tflcams.ldsys.co.uk/)
---
+++ tracker.e53228775c924f2e.js
@@ -0,0 +1,7 @@
+var sc_project=11383041; 
+var sc_invisible=1; 
+var sc_security="8002f6e1"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+  "https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost + 
+  "statcounter.com/counter/counter.js'></"+"script>");
